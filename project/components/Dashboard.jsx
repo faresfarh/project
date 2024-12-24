abbb404e6643be86e13bd46370c4391574b923b1
faresfarh <farsfarh52@gmail.com>
2024-12-25 00:39:05 +0200

    Update Dashboard.jsx

--- project/components/Dashboard.jsx
+++ project/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 export default function Dashboard({ role }) {
   return (
     <div className="flex">
-      <Sidebar role={role} />
+   
       <div className="flex-1 p-8">
         <h1 className="text-4xl">Welcome to {role} Dashboard</h1>
       </div>
